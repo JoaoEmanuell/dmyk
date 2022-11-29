0244a468bba83c5a4421a23484a74c14a7cf838d
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-11-29 17:44:57 -0300

    Notification > init, validate icon path

--- source_notification/notification.py
+++ source_notification/notification.py
@@ -3,18 +3,21 @@ from pynotifier import Notification as PyNotification
 from .interfaces import NotificationInterface
 
 class Notification(NotificationInterface):
-    def __init__(self, platform: str = None, title: str='', icon_path: str = None) -> None:
+    def __init__(self, platform: str = None, title: str='', \
+        icon_path: str = None) -> None:
         self.__platform = str(platform)
         self.__title = str(title)
         self.__desktop_platforms = ('win', 'linux', )
+        platforms_icon = {'linux': 'png', 'win': 'ico'}
 
         # Validate icon path
-        if self.__platform == 'linux' and icon_path != None:
-            self.__icon_path = f'{icon_path}.png'
-        elif self.__platform == 'win' and icon_path != None:
-            self.__icon_path = f'{icon_path}.ico'
-        else:
+        if icon_path == None:
             self.__icon_path = None
+        else:
+            if self.__platform in self.__desktop_platforms:
+                self.__icon_path = platforms_icon[self.__platform]
+            else:
+                self.__icon_path = None
 
     def send_notification(self, description: str = '', \
         duration: int = 3, urgency: str = 'normal') -> None:
