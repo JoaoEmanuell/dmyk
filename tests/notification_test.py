7a4a8bbf3598ade7d5d1fb658526e4aca07d5e87
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-11-29 17:34:17 -0300

    Update notification test

--- tests/notification_test.py
+++ tests/notification_test.py
@@ -6,14 +6,16 @@ from kivy.utils import platform
 from source_notification import Notification, notification
 
 def test_answer():
-    notification_obj = Notification(platform)
+    notification_obj = Notification(platform, 'Test')
 
     notification_obj.send_notification(
-        title='Test',
         description='Description'
     )
 
+    notification_obj = Notification('test', 'Test 2')
+    
+    notification_obj.send_notification('Description 2') # Not send
+
     notification.send_notification(
-        title='Test 2',
-        description='Description 2'
+        description='Description 3'
     )
