6512c0e2c47bd66d80f22068f0234b23d4d8e3ab
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-13 10:56:02 -0300

    playlist > added quality to playlist

--- source_download/download_playlist.py
+++ source_download/download_playlist.py
@@ -10,15 +10,16 @@ from .interfaces import DownloadPlaylistInterface, DownloadEssentialInterface
 from .message import Message
 
 class DownloadPlaylist(DownloadPlaylistInterface):
-    def __init__(self, playlistLink : list, convertToMp3  : bool) -> None:
-        self.__playlist = Playlist(playlistLink)
-        self.__CONVERT = convertToMp3
+    def __init__(self, playlist_link: list, convert_to_mp3: bool, quality: str) -> None:
+        self.__playlist = Playlist(playlist_link)
+        self.__CONVERT = convert_to_mp3
+        self.__quality = quality
 
-    def download_playlist(self, download : Type[DownloadEssentialInterface]):
+    def download_playlist(self, download: Type[DownloadEssentialInterface]):
         Message.set_output(f"Download da playlist iniciado!")
         for video in self.__playlist:
             if self.__CONVERT:
-                download(video, self.__CONVERT).download_audio()
+                download(video, self.__CONVERT, self.__quality).download_audio()
             else:
-                download(video, self.__CONVERT).download_video()
+                download(video, self.__CONVERT, self.__quality).download_video()
         Message.set_output("Download da Playlist concluído!")
