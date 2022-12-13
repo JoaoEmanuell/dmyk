bf74d5038c8c45a1404624975c592f286a08f5b0
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-13 10:53:15 -0300

    interfaces > all download interfaces modifed

--- source_download/interfaces/download_essential_interface.py
+++ source_download/interfaces/download_essential_interface.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class DownloadEssentialInterface(ABC):
     @abstractmethod
-    def __init__(self, link : str, mp3 : bool) -> None:
+    def __init__(self, link: str, mp3: bool, quality: str) -> None:
         raise NotImplementedError
 
     @abstractmethod
--- source_download/interfaces/download_interface.py
+++ source_download/interfaces/download_interface.py
@@ -3,9 +3,9 @@ from abc import ABC, abstractmethod
 class DownloadInterface(ABC) :
 
     @abstractmethod
-    def download(self, url : str) -> bytes :
+    def download(self, url: str) -> bytes :
         raise NotImplementedError
 
     @abstractmethod
-    def save_file(self, name : str, path : str, content : bytes) -> None :
+    def save_file(self, name: str, path: str, content: bytes) -> None :
         raise NotImplementedError
