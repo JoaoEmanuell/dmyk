b0d5607a8e1f8807d882e62cdda10aecbcc0c528
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-16 09:59:20 -0300

    main > custom thread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Global imports
-from threading import Thread
 from urllib.error import URLError
 from urllib.request import urlopen
 
@@ -12,6 +11,7 @@ from intent import Intent # Android intent
 from source_android import Android, service
 from source_api import ApiControl
 from source_ui import ui_drop_down_obj
+from source_thread import CustomThreadInterface, custom_thread_obj
 
 from source_download import (
     DownloadVideo, DownloadPlaylist, Message, MessageInterface
@@ -19,20 +19,31 @@ from source_download import (
 from download import DownloadVerify
 
 class Tela(Screen):
-    def __init__(self, message_class: MessageInterface, **kwargs):
+    def __init__(self, message_class: MessageInterface, custom_thread:  \
+        CustomThreadInterface, **kwargs):
+
         super().__init__(**kwargs)
         self.ids.link.text = Intent(platform).get_intent_text()
         self.__message_class = message_class
+        self.__custom_thread = custom_thread
         ApiControl()
 
     def main(self) -> None:
-        try :
+        try:
             urlopen('https://www.youtube.com')
         except URLError:
             self.__message_class.set_output('Sua conexão de internet está \
                 indisponível, por favor tente novamente')
         else:
-            self.start_download()
+            try:
+                if self.__custom_thread.is_alive():
+                    self.__custom_thread.kill()
+                    self.__custom_thread.join()
+                    self.__message_class.set_output('')
+                else:
+                    self.start_download()
+            except AssertionError: # Case the thread not created
+                self.start_download()
 
     def start_download(self):
         self.__message_class.set_output('')
@@ -40,13 +51,14 @@ class Tela(Screen):
         try:
             url = str(self.ids.link.text)
 
-            Thread(
+            self.__custom_thread.set_thread(
                 target=DownloadVerify.main, 
                 args=(
                     url, self.verify_mp3(), ui_drop_down_obj.get_text(),
                     DownloadVideo, DownloadPlaylist, 
                 )
-            ).start()
+            )
+            self.__custom_thread.start()
 
         except Exception as erro:
             self.__message_class.set_output(f'Alguma coisa deu errado!\nPor favor \
@@ -57,7 +69,7 @@ class Tela(Screen):
         mp4 = self.ids.mp4.state
         if mp3 == 'down':
             return True
-        elif mp4 == 'down' :
+        elif mp4 == 'down':
             return False
         else:
             return True
@@ -68,11 +80,15 @@ class Tela(Screen):
 
 class Main(App):
     def build(self) -> Screen:
-        return Tela(Message)
+        return Tela(Message, custom_thread_obj)
 
     def on_start(self): service.start_service()
 
-    def on_stop(self): service.stop_service()
+    def on_stop(self): 
+        service.stop_service()
+        print("Kill threads to stop application!")
+        custom_thread_obj.kill()
+        custom_thread_obj.join()
 
     def on_pause(self): return True
 
