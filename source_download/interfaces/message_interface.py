7c36df2f4815eebb580c13e4e5ba81ad26a41549
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-16 12:19:57 -0300

    Update message interface, set widget style

--- source_download/interfaces/message_interface.py
+++ source_download/interfaces/message_interface.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractclassmethod
 class MessageInterface(ABC):
     """This class send message to interface in main thread."""    
     @abstractclassmethod
-    def set_output(text: str='') -> None:
+    def set_output(cls, text: str='') -> None:
         """Set output text
 
         Args:
@@ -12,7 +12,7 @@ class MessageInterface(ABC):
         raise NotImplementedError()
 
     @abstractclassmethod
-    def set_progressbar(max: int=100, percent: int=0) -> None:
+    def set_progressbar(cls, max: int=100, percent: int=0) -> None:
         """Set progress bar
 
         Args:
@@ -22,7 +22,7 @@ class MessageInterface(ABC):
         raise NotImplementedError()
 
     @abstractclassmethod
-    def set_download_button_text(text: str='') -> None:
+    def set_download_button_text(cls, text: str='') -> None:
         """Set the download button text
 
         Args:
@@ -31,7 +31,19 @@ class MessageInterface(ABC):
         raise NotImplementedError()
 
     @abstractclassmethod
-    def set_out(text: str='') -> None:
+    def set_widget_style(cls, widget_id: str='', propriety: str='', \
+        style: str='') -> None:
+        """Set widget style
+
+        Args:
+            widget_id (str): Id on widget. Defaults to ''.
+            propriety (str, optional): Propriety to change. Defaults to ''.
+            style (str, optional): Style to apply, created on styles dict. Defaults to ''.
+        """        
+        raise NotImplementedError()
+
+    @abstractclassmethod
+    def set_out(cls, text: str='') -> None:
         """Set output text alias
 
         Args:
@@ -40,7 +52,7 @@ class MessageInterface(ABC):
         raise NotImplementedError()
 
     @abstractclassmethod
-    def set_pb(max: int=100, percent: int=0) -> None:
+    def set_pb(cls, max: int=100, percent: int=0) -> None:
         """Set progress bar alias
 
         Args:
@@ -50,10 +62,22 @@ class MessageInterface(ABC):
         raise NotImplementedError()
 
     @abstractclassmethod
-    def set_dbt(text: str='') -> None:
+    def set_dbt(cls, text: str='') -> None:
         """Set the download button text alias
 
         Args:
             text (str): New text to button. Defaults to ''.
+        """        
+        raise NotImplementedError()
+
+    @abstractclassmethod
+    def set_ws(cls, widget_id: str='', propriety: str='', \
+        style: str='') -> None:
+        """Set widget style
+
+        Args:
+            widget_id (str): Id on widget. Defaults to ''.
+            propriety (str, optional): Propriety to change. Defaults to ''.
+            style (str, optional): Style to apply, created on styles dict. Defaults to ''.
         """        
         raise NotImplementedError()
