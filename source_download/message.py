b8ab113321ce02c44c862dde7ae9a3f4fc199616
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-16 11:03:55 -0300

    Adequate message.py to message_interface.py

--- source_download/message.py
+++ source_download/message.py
@@ -8,11 +8,30 @@ from kivy.clock import mainthread
 from .interfaces import MessageInterface
 
 class Message(MessageInterface):
+    @classmethod
     @mainthread
-    def set_output(text: str=''):
+    def set_output(cls, text: str='') -> None:
         App.get_running_app().root.ids.output.text = str(text)
 
+    @classmethod
     @mainthread
-    def set_progressbar(max: int=100, percent: int=0):
+    def set_progressbar(cls, max: int=100, percent: int=0) -> None:
         App.get_running_app().root.ids.progressbar.max = int(max)
         App.get_running_app().root.ids.progressbar.value = int(percent)
+
+    @classmethod
+    @mainthread
+    def set_download_button_text(cls, text: str='') -> None:
+        App.get_running_app().root.ids.download_button.text = str(text)
+
+    @classmethod
+    def set_out(cls, text: str='') -> None:
+        cls.set_output(text)
+
+    @classmethod    
+    def set_pb(cls, max: int=100, percent: int=0) -> None:
+        cls.set_progressbar(max, percent)
+
+    @classmethod
+    def set_dbt(cls, text: str='') -> None:
+        cls.set_download_button_text(text)
