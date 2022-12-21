f74310eeaeca0756e8a555c75eec87a92dd6efb2
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-20 21:27:01 -0300

    Update download video interface

--- dmyk/source/download/interfaces/download_video_interface.py
+++ dmyk/source/download/interfaces/download_video_interface.py
@@ -1,15 +1,31 @@
 from abc import ABC, abstractmethod
 
+from .message_interface import MessageInterface
+from .download_essential_interface import DownloadEssentialInterface
+
 
 class DownloadVideoInterface(ABC):
     @abstractmethod
-    def __init__(self, link: str, mp3: bool, quality: str) -> None:
+    def __init__(
+        self,
+        link: str,
+        mp3: bool,
+        quality: str,
+        message: MessageInterface,
+        download_essential: DownloadEssentialInterface,
+    ) -> None:
+        """Init
+
+        Args:
+            link (str): link to YouTube vídeo
+            mp3 (bool): convert vídeo
+            quality (str): quality of download
+            message (MessageInterface): message class to send message for interface
+            download_essential (DownloadEssentialInterface): download essential object
+        """
         raise NotImplementedError
 
     @abstractmethod
-    def download_video(self) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
-    def download_audio(self) -> None:
-        raise NotImplementedError
+    def download(self) -> None:
+        """Start download"""
+        raise NotImplementedError()
