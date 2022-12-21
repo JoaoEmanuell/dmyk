63759918667af0abde5c50acd31d85ca5f789a6d
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-21 20:47:40 -0300

    Update _setup_local

--- _setup_local.py
+++ _setup_local.py
@@ -1,44 +1,56 @@
 from os.path import exists, join
-from os import mkdir, listdir
+from os import listdir
 from shutil import copytree
 from pathlib import Path
 from typing import Tuple
 
 
 def verify_virtual_env(path: str) -> bool:
+    """Verify if exists virtual env
+
+    Args:
+        path (str): Absolute path to project
+
+    Raises:
+        FileNotFoundError: Case the virtual env not found
+
+    Returns:
+        bool: True if virtual env exists
+    """
     if exists(path):
         return True
     raise FileNotFoundError("VIRTUAL ENV NOT FOUND!")
 
 
-def copy_external_modules(
-    absolute_path: str, new_directory: str, directories: Tuple[str]
-) -> bool:
-    external_modules_directory = join(absolute_path, new_directory, "external_modules")
+def copy_external_modules(absolute_path: str, directories: Tuple[Tuple[str]]) -> None:
+    """Copy external modules
 
-    if not exists(external_modules_directory):
-        print(f"Mkdir : {external_modules_directory}")
-        mkdir(external_modules_directory)
+    Args:
+        absolute_path (str): Absolute path to project
+        directories (Tuple[Tuple[str]]): Tuple of tuples, the first tuple is a
+        module to copy, the second tuple is a absolute path to destination the module.
+    """
 
     path_to_directory_packages = join(
         absolute_path, "lib", listdir(f"{absolute_path}lib")[0], "site-packages", ""
     )
 
     print(f"Python packages : {path_to_directory_packages}")
-    external_modules_path = join(absolute_path, new_directory)
-    print(f"Path to external: {external_modules_path}")
 
     for directory in directories:
 
-        path = join(path_to_directory_packages, directory, "")
+        external_modules_path = join(absolute_path, directory[1])
+        print(f"Path to external: {external_modules_path}")
+
+        path = join(path_to_directory_packages, directory[0], "")
 
         try:
-            copytree(path, join(external_modules_path, directory), False, None)
-            print(f"Package {directory} copied with success")
+            copytree(path, join(external_modules_path, directory[0]), False, None)
+            print(f"Package {directory[0]} copied with success")
         except FileNotFoundError:
-            print(f"Package not found : {directory}")
+            print(f"Package not found : {directory[0]}")
         except FileExistsError:
-            print(f"Exists : {directory}")
+            print(f"Exists : {directory[0]}")
         except Exception as err:
             print(f"ERR : {err}")
 
@@ -46,8 +58,9 @@ def copy_external_modules(
 if __name__ == "__main__":
     absolute_path = join(Path().absolute(), "")
     verify_virtual_env(join(absolute_path, "bin", "pip"))
+
+    path_to_download = join(absolute_path, "dmyk", "source", "download")
     copy_external_modules(
         absolute_path=absolute_path,
-        new_directory=join("dmyk", "source", "download"),
-        directories=("pytube",),
+        directories=(("pytube", path_to_download),),
     )
