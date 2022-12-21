bc6841c77fc4f7141a4298e40f093aa8348ecb24
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-12-20 21:23:07 -0300

    Adequated download playlist to interface

--- dmyk/source/download/download_playlist.py
+++ dmyk/source/download/download_playlist.py
@@ -1,25 +1,39 @@
 # Global imports
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
-from typing import Type
 
 # Local imports
 
 from .pytube import Playlist
-from ..interfaces import DownloadPlaylistInterface, DownloadEssentialInterface
-from .message import Message
+from .interfaces import (
+    DownloadPlaylistInterface,
+    DownloadEssentialInterface,
+    MessageInterface,
+    DownloadVideoInterface,
+)
+
 
 class DownloadPlaylist(DownloadPlaylistInterface):
-    def __init__(self, playlist_link: list, convert_to_mp3: bool, quality: str) -> None:
+    def __init__(
+        self,
+        playlist_link: list,
+        convert_to_mp3: bool,
+        quality: str,
+        message: MessageInterface,
+        download_essential: DownloadEssentialInterface,
+    ) -> None:
         self.__playlist = Playlist(playlist_link)
         self.__CONVERT = convert_to_mp3
         self.__quality = quality
+        self.__message = message
+        self.__download_essential = download_essential
 
-    def download_playlist(self, download: Type[DownloadEssentialInterface]):
-        Message.set_output(f"Download da playlist iniciado!")
+    def download_playlist(self, download: DownloadVideoInterface):
+        self.__message.set_out(f"Download da playlist iniciado!")
         for video in self.__playlist:
             if self.__CONVERT:
                 download(video, self.__CONVERT, self.__quality).download_audio()
             else:
                 download(video, self.__CONVERT, self.__quality).download_video()
-        Message.set_output("Download da Playlist concluído!")
+        self.__message.set_out("Download da Playlist concluído!")
