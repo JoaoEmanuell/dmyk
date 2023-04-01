34198c6690e222a7bf6a2e8e60d557506837e820
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2023-04-01 17:13:07 -0300

    Moved download.js

--- js/download.js
+++ js/download.js
@@ -17,18 +17,19 @@ async function request_get(url=''){
 }
 
 request_get(release_api_endpoint).then((data) => {
+    const div_to_body_request = document.createElement("div")
+    const link_to_download = document.createElement('a')
+    link_to_download.classList.add('text-success')
 
     if (data.message == "Not Found"){
-        window.alert("Não existe lançamentos!")
+        div_to_body_request.innerHTML = "<h2>Não existe lançamentos disponíveis!</h2>"
+        div_to_body_request.classList.add('text-danger')
     } else{
-        const div_to_body_request = document.createElement("div")
-        div_to_body_request.classList.add()
+        release_div.classList.add('shadow-lg', 'p-4', 'rounded', 'bg-light')
         div_to_body_request.innerHTML = `<pre>${data.body}</pre>`
-
-        const link_to_download = document.createElement('a')
+        div_to_body_request.classList.add('text-dark')
         link_to_download.innerHTML = 'Clique aqui para baixar a nova versão!'
         link_to_download.href = data.assets[0].browser_download_url
-        
-        release_div.append(div_to_body_request, link_to_download)
     }
+    release_div.append(div_to_body_request, link_to_download)
 })
