e3e38b5350e120e6a6f245870fc3d7f3b4bfd395
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2023-04-09 15:19:35 -0300

    style: DropDown
    Style on DropDown and DropDown items

--- dmyk/source/ui/ui_drop_down.py
+++ dmyk/source/ui/ui_drop_down.py
@@ -2,6 +2,7 @@ from typing import Callable, Optional
 
 from kivy.metrics import dp
 from kivymd.uix.menu.menu import MDDropdownMenu
+from kivymd.color_definitions import colors
 
 from .interfaces import UiDropDownInterface
 
@@ -11,10 +12,13 @@ class UiDropDown(MDDropdownMenu, UiDropDownInterface):
         super().__init__(**kwargs)
         self.__qualities = ("720p", "480p", "360p", "240p", "144p")
         self.size_hint_y = None
-        self.max_height = 2000
+        self.max_height = dp(300)
         self.__text = "720p"
-        self.width_mult = 4
+        self.width_mult = 1.25
+        self.background_color = colors['Blue']['700']
         self.border_margin = dp(4)
+        self.position = 'center'
+        
 
     def create_ui(self, release_callback: Optional[Callable] = None) -> None:
         items = []
@@ -23,6 +27,9 @@ class UiDropDown(MDDropdownMenu, UiDropDownInterface):
                 "viewclass": "OneLineListItem",
                 "height": dp(56),
                 "text": f"{quality}",
+                "divider": 'Full',
+                "divider_color": (1, 1, 1, 1),
+                "divider_height": 1,
                 "on_release": lambda x=f"{quality}": (
                     self.__set_text(x),
                     release_callback(text=quality) if release_callback else None,
