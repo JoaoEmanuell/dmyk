061b33068c19957dcdf8955c2df550b879c59e5d
JoaoEmanuell <https://JoaoEmanuell@github.com>
2023-07-07 14:50:43 -0300

    black formatter

--- tests/source/download/download_essential_test.py
+++ tests/source/download/download_essential_test.py
@@ -1,22 +1,27 @@
 from sys import path
 from getpass import getuser
-path.append('..')
+
+path.append("..")
 
 from kivy.utils import platform
-from dmyk.source import DownloadEssential, DownloadContent, DownloadEssentialInterface, ApiControl
+from dmyk.source import (
+    DownloadEssential,
+    DownloadContent,
+    DownloadEssentialInterface,
+    ApiControl,
+)
 from dmyk.source.download.pytube import Stream
+
 
 def test_answer() -> None:
     download_essential = DownloadEssential(
-        api_control=ApiControl(),
-        download_content=DownloadContent,
-        message=None
+        api_control=ApiControl(), download_content=DownloadContent, message=None
     )
     assert isinstance(download_essential, DownloadEssentialInterface)
 
     # Verify If File Not Exists
-    assert download_essential.verify_if_file_not_exists(True, Stream, '')
-    assert download_essential.verify_if_file_not_exists(False, Stream, '')
+    assert download_essential.verify_if_file_not_exists(True, Stream, "")
+    assert download_essential.verify_if_file_not_exists(False, Stream, "")
 
     # Get download path
 
@@ -26,4 +31,3 @@ def test_answer() -> None:
         "linux": "/home/%s" % getuser(),
     }
     assert path_to_save == platforms_path[platform]
-    
