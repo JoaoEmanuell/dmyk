de68f8f481e9c2762c88b94dff336898f0df3f49
JoaoEmanuell <81983803+JoaoEmanuell@users.noreply.github.com>
2024-01-17 10:50:29 -0300

    refactor: timeout and api endpoint
    change the timeout in download video and use a new url in api control

--- dmyk/source/api/api_control.py
+++ dmyk/source/api/api_control.py
@@ -8,7 +8,8 @@ from .interfaces import ApiControlInterface
 
 class ApiControl(ApiControlInterface):
     def __init__(self) -> None:
-        self.__endpoint = "https://mp3api-pv49xe5o.b4a.run/api/"
+        self.__endpoint = "https://mp3-api.fly.dev/api/"
+        self.__timeout = 60 # 60 seconds
         self.private__start_api()
 
     def private__start_api(self) -> None:
@@ -16,25 +17,25 @@ class ApiControl(ApiControlInterface):
 
     def upload(self, file_path: str) -> Dict[str, str]:
         with open(file_path, "rb") as file:
-            response = post(f"{self.__endpoint}upload/", files={"file": file})
+            response = post(f"{self.__endpoint}upload/", files={"file": file}, timeout=self.__timeout)
             if response.status_code == 200:
                 return response.json()
             else:
                 raise Exception(f"Error:{response.status_code}")
 
     def get_status(self, hash: str) -> Dict[str, str]:
-        response = get(f"{self.__endpoint}status/{hash}")
+        response = get(f"{self.__endpoint}status/{hash}", timeout=self.__timeout)
         if response.status_code == 200:
             return response.json()
         else:
             raise Exception(f"Error:{response.status_code}")
 
     def get_file(self, hash: str) -> Dict[str, str]:
-        response = get(f"{self.__endpoint}converteds/{hash}")
+        response = get(f"{self.__endpoint}converteds/{hash}", timeout=self.__timeout)
         if response.status_code == 200:
             return response.json()
         else:
             raise Exception(f"Error:{response.status_code}")
 
     def delete_file(self, hash: str) -> None:
-        get(f"{self.__endpoint}delete/{hash}")
+        get(f"{self.__endpoint}delete/{hash}", timeout=self.__timeout)
